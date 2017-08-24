19c857dc47e743f4748a4f7b4aaff3126a6dadcf
Allegories <aleegoris@gmail.com>
2017-08-24 12:22:29 +0700

    changes added

---
+++ Demo1-GuessTheLanguage/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+guesses = ['Python', 'Java', 'C++']
+
+@app.route('/')
+def index():
+    return"<h1>Guess The language!</h1>"
+
+@app.route('/guess/<int:id>')
+def guess(id):
+    return ('<h1>Guess The language!</h1>'
+            '<p>My Guess: {0}</p>').format(guesses[id])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)    
